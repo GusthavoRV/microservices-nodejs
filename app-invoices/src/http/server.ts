c9306a5865fd4ea2272f5d67960a97010b9d67c9
GusthavoRV <gusthavorangel@gmail.com>
2025-08-24 12:59:46 -0300

    jaeger tracing config

--- app-invoices/src/http/server.ts
+++ app-invoices/src/http/server.ts
@@ -1,8 +1,7 @@
+import "@opentelemetry/auto-instrumentations-node/register";
 import "../broker/subscriber.ts";
-
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
-
 import {
   serializerCompiler,
   validatorCompiler,
